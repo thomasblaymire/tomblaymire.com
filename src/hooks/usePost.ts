489959bd7a09027994ed4cc324f2c8a421a329a2
Tom Blaymire <thomasblaymire@gmail.com>
2022-12-14 11:23:25 +0000

    feat(app): create a usePost hook for the blog

---
+++ src/hooks/usePost.ts
@@ -0,0 +1,40 @@
+import { createClient, Entry } from 'contentful';
+import { useEffect, useState } from 'react';
+
+export type BlogPostFields = {
+  title: string;
+  body: any;
+};
+
+export const usePost = (slug: string | undefined) => {
+  const [post, setPost] = useState<null | Entry<BlogPostFields>>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<Error | null>(null);
+
+  const client = createClient({
+    space: import.meta.env.VITE_CONTENTFUL_SPACE_ID,
+    accessToken: import.meta.env.VITE_CONTENTFUL_ACCESS_TOKEN,
+  });
+
+  useEffect(() => {
+    const getSinglePost = async (slug: string | undefined) => {
+      try {
+        const item = await client.getEntries({
+          'fields.slug': slug,
+          content_type: 'blogPost',
+        });
+        setPost(item.items[0] as Entry<BlogPostFields>);
+      } catch (err) {
+        const error = err as Error;
+        console.error(
+          `Error: unable to load post name=${error.name} message=${error.message} stack=${error.stack}`,
+        );
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    getSinglePost(slug);
+  }, []);
+  return { post, error, loading };
+};
