07f200b7c9135f29a6ff8219d6e4256738275394
Tom Blaymire <thomasblaymire@gmail.com>
2022-12-28 15:07:42 +0000

    feat(app): add react google analytics for tracking

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactGA from 'react-ga4';
 
 import App from './App';
+
+ReactGA.initialize(import.meta.env.VITE_GOOGLE_ANALYTICS_TAG);
 
 ReactDOM.render(
   <React.StrictMode>
