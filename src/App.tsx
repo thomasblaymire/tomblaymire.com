8b3f9f60301d7bcdae27ba94740a33129abf4f21
Tom Blaymire <thomasblaymire@gmail.com>
2025-11-11 15:21:26 +0000

    chore: remove styled-components dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,15 @@
 import { Suspense } from 'react';
-import { ThemeProvider } from 'styled-components';
 
 import { Loading } from '@/components/loading';
-import { darkTheme, GlobalStyle, lightTheme, THEME } from '@/helpers/style';
 import { useLightMode } from '@/hooks/useLightMode';
 import { BaseApp } from '@/pages';
 
 function App() {
   const [theme, toggleTheme] = useLightMode();
-  const themePreference = theme === THEME.LIGHT ? darkTheme : lightTheme;
 
   return (
     <Suspense fallback={<Loading />}>
-      <ThemeProvider theme={themePreference}>
-        <GlobalStyle />
-        <BaseApp toggleTheme={toggleTheme} theme={theme} />
-      </ThemeProvider>
+      <BaseApp toggleTheme={toggleTheme} theme={theme} />
     </Suspense>
   );
 }
