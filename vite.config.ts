f3014b31e8afedad6644a35c52e2e86275f7c000
Tom Blaymire <thomasblaymire@gmail.com>
2025-11-11 15:43:27 +0000

    chore: fix security vulnerabilities and migrate to husky

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
 import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'url';
 import { defineConfig } from 'vite';
-import Markdown from 'vite-plugin-react-markdown';
 
 export default defineConfig({
   plugins: [
-    Markdown(),
     react({
-      include: [/\.tsx$/, /\.ts$/, /\.md$/],
+      include: [/\.tsx$/, /\.ts$/],
     }),
   ],
   resolve: {
