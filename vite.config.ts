a795e9f33edc933105f97b6ad44e0e9fd652605e
Tom Blaymire <thomasblaymire@gmail.com>
2026-01-15 22:47:44 +0000

    configure vite for mdx

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
+import mdx from '@mdx-js/rollup';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'url';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [
+    mdx(),
     react({
-      include: [/\.tsx$/, /\.ts$/],
+      include: [/\.tsx$/, /\.ts$/, /\.mdx$/],
     }),
   ],
   resolve: {
@@ -18,8 +20,6 @@ export default defineConfig({
       output: {
         manualChunks: {
           'react-vendor': ['react', 'react-dom', 'react-router-dom'],
-          'contentful-vendor': ['contentful'],
-          'markdown-vendor': ['react-markdown'],
         },
       },
     },
